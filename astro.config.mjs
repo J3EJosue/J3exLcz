68c3c0144603630c2a902b49db856ee327ffcef8
J3EJosue <Josuemanuelcruzzz@gmail.com>
2024-04-23 15:48:41 -0600

    S

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-import cloudflare from "@astrojs/cloudflare/pages";
-
-const platform = process.env.DEPLOY_PLATFORM || 'vercel';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "dist", // Directorio de salida para Cloudflare Pages
-  adapter: platform === 'vercel' ? vercel() : cloudflare()
+  output: "dist", // Directorio de salida para Vercel
+  adapter: vercel()
 });
