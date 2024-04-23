e07317b996c4f8891a22442562c89a052176143d
J3EJosue <Josuemanuelcruzzz@gmail.com>
2024-04-23 16:00:35 -0600

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "dist", // Directorio de salida para Vercel
-  adapter: vercel()
+  integrations: [tailwind(), react()]
 });
