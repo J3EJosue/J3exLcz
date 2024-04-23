e8a9a1b67f8b984eb3c0808c01796b969ce11a8f
J3EJosue <Josuemanuelcruzzz@gmail.com>
2024-04-23 15:33:07 -0600

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare/pages";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  output: "dist", // Directorio de salida para Cloudflare Pages
+  adapter: cloudflare()
 });
